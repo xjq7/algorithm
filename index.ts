a8d4f11f05fddc22b0f25070c9f296d72758495f
xjq <lebnizjoan@gmail.com>
2022-11-05 12:12:04 +0800

    build script

---
+++ index.ts
@@ -0,0 +1,9 @@
+import Kmp from './Kmp/Kmp';
+import Plain from './Kmp/Plain';
+import PriorityQueueHeap from './PriorityQueue/PriorityQueue-Heap';
+
+export default {
+  Kmp,
+  Plain,
+  PriorityQueueHeap,
+};
